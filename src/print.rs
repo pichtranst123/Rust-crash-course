459683c2d32739118fa5a9ec7f40e0b648dda83e
Peter <thaiphuongn1@gmail.com>
2020-10-10 15:38:43 +0700

    rust print

---
+++ src/print.rs
@@ -0,0 +1,27 @@
+pub fn run() {
+    // Print to console
+    println!("Hello from the print.rs file");
+
+    // Basic Formatting
+    println!("Number: {}", 1);
+    println!("{} is from {}", "Peter", "VN")
+
+    // Positional Arguments
+    println!("{0} is from {1} and {0} like to {2}", "Peter", "Vn", "code");
+
+    // Named Arguments
+    println!(
+        "{name} like to play {activity}",
+        name = "John",
+        activity = "baseball"
+    );
+
+    // Placeholder traits
+    println!("Binary {:b} Hex {:x} Octal: {:o}", 10, 10, 10);
+
+    // Placeholder for debug trait
+    println!("{:?}", (12, true, "hello"));
+
+    // Basic math
+    println!("10 + 10 = {}", 10 + 10);
+}
