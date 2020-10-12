c4e63e4e265e70d1a7f1ab249e65715b49a44344
Peter <thaiphuongn1@gmail.com>
2020-10-12 21:48:29 +0700

    tuples

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 // mod print;
 // mod vars;
 // mod types;
-mod strings;
+// mod strings;
+mod tuples;
 
 fn main() {
     // println!("Hello, world!");
     // print::run();
     // vars::run();
     // types::run();
-    strings::run();
+    // strings::run();
+    tuples::run();
 }
